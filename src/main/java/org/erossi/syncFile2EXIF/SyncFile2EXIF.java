81f91a5e5b8941c12ec6a76e97c8dc88f68bcd84
Emanuele Rossi <emanuele.rossi@gmail.com>
2018-10-15 09:41:09 +0200

    Added setTimes feature and a lot of bugfix!

--- src/main/java/org/erossi/syncFile2EXIF/SyncFile2EXIF.java
+++ src/main/java/org/erossi/syncFile2EXIF/SyncFile2EXIF.java
@@ -13,12 +13,14 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Optional;
+import java.util.Random;
 import java.util.TimeZone;
 import java.util.jar.JarInputStream;
 import java.util.stream.StreamSupport;
 
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.Tag;
 
@@ -26,6 +28,7 @@ import org.apache.commons.io.FilenameUtils;
 
 public class SyncFile2EXIF {
 
+    private final static String TIME_ZONE = "GMT";
     public static void main(String[] args) {  
 
       SyncFile2EXIF main = new SyncFile2EXIF();
@@ -41,92 +44,116 @@ public class SyncFile2EXIF {
         System.out.format("\tUsage: java -jar <jarFileName> [command] [parameters]\n");
         System.out.format("\t\t [command] -> help\n");   
         System.out.format("\t\t [command] -> times java -jar <jarFileName> times <directory> <fileNamePattern>\n");   
-        System.out.format("\t\t\t ex.: java -jar <jarFileName> times . *.jpg\n");     
+        System.out.format("\t\t\t ex.: java -jar <jarFileName> times . .jpg\n");     
         System.out.format("\t\t [command] -> name java -jar <jarFileName> name <directory> <fileNamePattern>\n");   
-        System.out.format("\t\t\t ex.: java -jar <jarFileName> name . *.jpg\n");                        
+        System.out.format("\t\t\t ex.: java -jar <jarFileName> name . .jpg\n");                       
+        System.out.format("\t\t [command] -> setTimes java -jar <jarFileName> setTimes <directory> <fileNamePattern> <yyyyMMddHHmmss>\n");   
+        System.out.format("\t\t\t ex.: java -jar <jarFileName> setTimes . .jpg 20181225120000\n");          
       }
       if (args[0].contains("times")) {
         if (args.length < 3 || args[1].isEmpty() || args[2].isEmpty()) {
           System.out.format("\tNo file(s) specified... try \"help\" command for instructions.\n");
         } else {
           File inputDir = new File(args[1]);
-          String filePattern = args[2].replaceAll("\\*", "");
+          String filePattern = args[2];
           Arrays.stream(inputDir.listFiles())
             .filter(f -> f.getName().contains(filePattern))
-            .forEach(f -> main.syncTimes(f));
+            .forEach(f -> main.setTimes(f, main.getEXIFOriginalDateTime(f)));
         }
       }    
       if (args[0].contains("name")) {
-        if (args.length < 3 || args[1].isEmpty() || args[2].isEmpty()) {
+        if (args.length < 3 || args[1].isEmpty() || args[2].isEmpty()) { 
+          System.out.format("\tNo file(s) specified... try \"help\" command for instructions.\n");
+        } else {      
+          File inputDir = new File(args[1]);
+          String filePattern = args[2];
+          Arrays.stream(inputDir.listFiles())
+            .filter(f -> f.getName().contains(filePattern))
+            .forEach(f -> main.setName(f, main.getEXIFOriginalDateTime(f)));          
+        }
+      } 
+      if (args[0].contains("setTimes")) {
+        if (args.length < 3 || args[1].isEmpty() || args[2].isEmpty() || args[3].isEmpty()) {
           System.out.format("\tNo file(s) specified... try \"help\" command for instructions.\n");
         } else {
           File inputDir = new File(args[1]);
-          String filePattern = args[2].replaceAll("\\*", "");
-          Arrays.stream(inputDir.listFiles())
-            .filter(f -> f.getName().contains(filePattern))
-            .forEach(f -> main.syncName(f));
+          String filePattern = args[2];
+          try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+            sdf.setTimeZone(TimeZone.getTimeZone(SyncFile2EXIF.TIME_ZONE));          
+            Date time = sdf.parse(args[3]);
+            Arrays.stream(inputDir.listFiles())
+              .filter(f -> f.getName().contains(filePattern))
+              .forEach(f -> main.setTimes(f, time));
+          } catch (ParseException pe) {
+            System.err.format("%s |%s|", "ERROR! Wrong input date string!  :(", args[3]);       
+          }
         }
-      }        
+      }              
     }
 
-    public void syncTimes(File inputFile) {
+    public void setTimes(File inputFile, Date date) {
       try {        
-        Date exifDateTime = this.getEXIFOriginalDateTime(inputFile);
-        if (exifDateTime != null) {          
+        if (date != null) {          
           BasicFileAttributeView inputFileView = Files.getFileAttributeView(Paths.get(inputFile.getAbsolutePath()), BasicFileAttributeView.class);
-          inputFileView.setTimes(FileTime.fromMillis(exifDateTime.getTime()), FileTime.fromMillis(exifDateTime.getTime()), FileTime.fromMillis(exifDateTime.getTime()));
+          inputFileView.setTimes(FileTime.fromMillis(date.getTime()), FileTime.fromMillis(date.getTime()), FileTime.fromMillis(date.getTime()));
         } else {
           System.out.format("%s |%s| %s |%s|", "Cannot find \"Date/Time Original\" tag into EXIF metadata of file", inputFile.getAbsolutePath());          
         }
       } catch (IOException ioe) {
         System.err.format("%s %s |%s|", "ERROR! Reading file :(", ioe, inputFile.getAbsolutePath());        
-      } catch (ImageProcessingException ipe) {
-        System.err.format("%s %s |%s|", "ERROR! Getting Image Metadata :(", ipe, inputFile.getAbsolutePath());       
-      } catch (ParseException pe) {
-        System.err.format("%s %s |%s|", "ERROR! Getting Date/Time :(", pe, inputFile.getAbsolutePath());       
-      }
+      } 
     }
 
-    public void syncName(File inputFile) {
+    public void setName(File inputFile, Date date) {
       try {
-        Date exifDateTime = this.getEXIFOriginalDateTime(inputFile);
-        if (exifDateTime != null) {
-          String newNameBase = new SimpleDateFormat("yyyyMMddHHmmss").format(exifDateTime);
+        if (date != null) {
+          String newNameBase = new SimpleDateFormat("yyyyMMddHHmmss").format(date);
           String inputFileExtension = FilenameUtils.getExtension(inputFile.getAbsolutePath());
           if (inputFileExtension == null | inputFileExtension == "") {
             inputFileExtension = "";
           } else {
             inputFileExtension = ".".concat(inputFileExtension);
           }
-          Path inputFilePath = Paths.get(inputFile.getAbsolutePath());
-          Files.move(inputFilePath, inputFilePath.resolveSibling(newNameBase.concat(inputFileExtension)));
+          Path inputFilePath = Paths.get(inputFile.getAbsolutePath());               
+          Path outputFilePath = inputFilePath.resolveSibling(newNameBase.concat(inputFileExtension));
+          if (outputFilePath.toFile().exists()) {            
+            newNameBase = newNameBase.concat(String.format("-%04d", new Random().nextInt(1000)));
+            outputFilePath = inputFilePath.resolveSibling(newNameBase.concat(inputFileExtension));
+          }
+          Files.move(inputFilePath, outputFilePath);
         } else {
-          System.out.format("%s |%s| %s |%s|", "Cannot find \"Date/Time Original\" tag into EXIF metadata of file", inputFile.getAbsolutePath());          
+          System.out.format("%s |%s|", "Cannot find \"Date/Time Original\" tag into EXIF metadata of file", inputFile.getAbsolutePath());          
         }
       } catch (IOException ioe) {
         System.err.format("%s %s |%s|", "ERROR! Reading file :(", ioe, inputFile.getAbsolutePath());        
-      } catch (ImageProcessingException ipe) {
-        System.err.format("%s %s |%s|", "ERROR! Getting Image Metadata :(", ipe, inputFile.getAbsolutePath());       
-      } catch (ParseException pe) {
-        System.err.format("%s %s |%s|", "ERROR! Getting Date/Time :(", pe, inputFile.getAbsolutePath());       
-      }
+      } 
     }
 
-    private Date getEXIFOriginalDateTime(File f) throws ImageProcessingException, IOException, ParseException {
+    public Date getEXIFOriginalDateTime(File f) {
       Date exifDateTime = null;
-      Metadata metadata = ImageMetadataReader.readMetadata(f);      
-      Optional<Tag> o_dateTime = StreamSupport.stream(metadata.getDirectories().spliterator(), false)
-        .flatMap(d -> d.getTags().stream())
-        .distinct()
-        //.forEach(t -> System.out.format("[%s] = |%s|\n", t.getTagName(), t.getDescription()))
-        .filter(t -> t.getTagName().equalsIgnoreCase("Date/Time Original"))
-        .findFirst();    
-      if (o_dateTime.isPresent()) {
-        String exifDateTimeString = o_dateTime.get().getDescription();
-        DateFormat exifDateTimeStringFormat = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");  
-        exifDateTimeStringFormat.setTimeZone(TimeZone.getTimeZone("Europe/Rome"));                  
-        exifDateTime = exifDateTimeStringFormat.parse(exifDateTimeString);         
-      }
+
+      try {
+        Metadata metadata = ImageMetadataReader.readMetadata(f);      
+        Optional<Tag> o_dateTime = StreamSupport.stream(metadata.getDirectories().spliterator(), false)
+          .flatMap(d -> d.getTags().stream())
+          .distinct()
+          //.forEach(t -> System.out.format("[%s] = |%s|\n", t.getTagName(), t.getDescription()))
+          .filter(t -> t.getTagName().equalsIgnoreCase("Date/Time Original"))
+          .findFirst();    
+        if (o_dateTime.isPresent()) {
+          String exifDateTimeString = o_dateTime.get().getDescription();
+          DateFormat exifDateTimeStringFormat = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");                    
+          exifDateTimeStringFormat.setTimeZone(TimeZone.getTimeZone(SyncFile2EXIF.TIME_ZONE));            
+          exifDateTime = exifDateTimeStringFormat.parse(exifDateTimeString);         
+        }
+      } catch (ImageProcessingException ipe) {
+        System.err.format("%s %s |%s|", "ERROR! Getting Image Metadata :(", ipe, f.getAbsolutePath());       
+      } catch (ParseException pe) {
+        System.err.format("%s %s |%s|", "ERROR! Getting Date/Time :(", pe, f.getAbsolutePath());       
+      } catch (IOException ioe) {
+        System.err.format("%s %s |%s|", "ERROR! Reading file :(", ioe, f.getAbsolutePath());        
+      } 
       return exifDateTime;
     }
 }
--- src/test/java/SyncFile2EXIFTest.java
+++ src/test/java/SyncFile2EXIFTest.java
@@ -11,6 +11,7 @@ import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
 import org.erossi.syncFile2EXIF.SyncFile2EXIF;
@@ -41,7 +42,7 @@ public class SyncFile2EXIFTest {
     ExpectedException ioe = ExpectedException.none();
 
     ioe.expect(IOException.class);
-    tmc.syncTimes(new File(".\\build\\resources\\test\\NonExistentFile.jpg"));
+    tmc.setTimes(new File(".\\build\\resources\\test\\NonExistentFile.jpg"), new Date());
   }
 
   @Test
@@ -49,31 +50,31 @@ public class SyncFile2EXIFTest {
     ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     System.setErr(new PrintStream(outContent));
 
-    tmc.syncTimes(new File(".\\build\\resources\\test\\sample-02.txt"));
+    tmc.setTimes(new File(".\\build\\resources\\test\\sample-02.txt"), new Date());
     assertNotNull(outContent);
   }  
 
   @Test
   public void testSamples() throws Exception {
-    File copy01 = testFolder.newFile("test-01.jpg");  
+    File copy01 = testFolder.newFile("sample-01.jpg");  
     FileUtils.copyFile(new File(".\\build\\resources\\test\\sample-01.jpg"), copy01);
-    tmc.syncTimes(copy01);
+    tmc.setTimes(copy01, tmc.getEXIFOriginalDateTime(copy01));
     BasicFileAttributes attr01 = Files.readAttributes(Paths.get(copy01.getAbsolutePath()), BasicFileAttributes.class); 
     assertEquals(Instant.parse("2016-07-10T14:53:51Z"), attr01.lastModifiedTime().toInstant()); 
     assertEquals(Instant.parse("2016-07-10T14:53:51Z"), attr01.lastAccessTime().toInstant()); 
     assertEquals(Instant.parse("2016-07-10T14:53:51Z"), attr01.creationTime().toInstant()); 
 
-    File copy02 = testFolder.newFile("test-02.jpg");  
+    File copy02 = testFolder.newFile("sample-02.jpg");  
     FileUtils.copyFile(new File(".\\build\\resources\\test\\sample-02.jpg"), copy02);
-    tmc.syncTimes(copy02);
+    tmc.setTimes(copy02, tmc.getEXIFOriginalDateTime(copy02));
     BasicFileAttributes attr02 = Files.readAttributes(Paths.get(copy02.getAbsolutePath()), BasicFileAttributes.class); 
     assertEquals(Instant.parse("2013-10-06T15:38:59Z"), attr02.lastModifiedTime().toInstant());     
     assertEquals(Instant.parse("2013-10-06T15:38:59Z"), attr02.lastAccessTime().toInstant()); 
     assertEquals(Instant.parse("2013-10-06T15:38:59Z"), attr02.creationTime().toInstant()); 
 
-    File copy03 = testFolder.newFile("test-03.jpg");  
+    File copy03 = testFolder.newFile("sample-03.jpg");  
     FileUtils.copyFile(new File(".\\build\\resources\\test\\sample-03.jpg"), copy03);    
-    tmc.syncTimes(copy03);
+    tmc.setTimes(copy03, tmc.getEXIFOriginalDateTime(copy03));
     BasicFileAttributes attr03 = Files.readAttributes(Paths.get(copy03.getAbsolutePath()), BasicFileAttributes.class); 
     assertEquals(Instant.parse("2013-10-06T15:38:50Z"), attr03.lastModifiedTime().toInstant()); 
     assertEquals(Instant.parse("2013-10-06T15:38:50Z"), attr03.lastAccessTime().toInstant()); 
@@ -82,7 +83,12 @@ public class SyncFile2EXIFTest {
 
   @Test
   public void testMain_Help() {
+    ByteArrayOutputStream sysOut = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(sysOut));
+
     SyncFile2EXIF.main(new String[] { "help" });
+
+    assertTrue(sysOut.toString().contains("Usage:"));
   }
 
   @Test
@@ -92,14 +98,14 @@ public class SyncFile2EXIFTest {
   
   @Test
   public void testMain_Times() throws Exception {
-    File copy01 = testFolder.newFile("test-01.jpg");  
+    File copy01 = testFolder.newFile("sample-01.jpg");  
     FileUtils.copyFile(new File(".\\build\\resources\\test\\sample-01.jpg"), copy01);   
-    File copy02 = testFolder.newFile("test-02.jpg");  
+    File copy02 = testFolder.newFile("sample-02.jpg");  
     FileUtils.copyFile(new File(".\\build\\resources\\test\\sample-02.jpg"), copy02);    
-    File copy03 = testFolder.newFile("test-03.jpg");  
+    File copy03 = testFolder.newFile("sample-03.jpg");  
     FileUtils.copyFile(new File(".\\build\\resources\\test\\sample-03.jpg"), copy03);    
 
-    SyncFile2EXIF.main(new String[] { "times", testFolder.getRoot().getAbsolutePath(), "*.jpg"});
+    SyncFile2EXIF.main(new String[] { "times", testFolder.getRoot().getAbsolutePath(), "*.jpg"} );
 
     BasicFileAttributes attr01 = Files.readAttributes(Paths.get(copy01.getAbsolutePath()), BasicFileAttributes.class); 
     assertEquals(Instant.parse("2016-07-10T14:53:51Z"), attr01.lastModifiedTime().toInstant());    
@@ -117,12 +123,38 @@ public class SyncFile2EXIFTest {
 
   @Test
   public void testMain_Name() throws Exception {
-    File copy01 = testFolder.newFile("test-01.jpg");  
+    File copy01 = testFolder.newFile("sample-01.jpg");  
     FileUtils.copyFile(new File(".\\build\\resources\\test\\sample-01.jpg"), copy01);       
 
-    SyncFile2EXIF.main(new String[] { "name", testFolder.getRoot().getAbsolutePath(), copy01.getName()});
+    SyncFile2EXIF.main(new String[] { "name", testFolder.getRoot().getAbsolutePath(), copy01.getName() });
 
     File outputFile = Arrays.stream(testFolder.getRoot().listFiles()).findFirst().get();
     assertTrue(outputFile.getName().equalsIgnoreCase("20160710165351.jpg"));
+  }
+
+  @Test
+  public void testMain_NameDuplicated() throws Exception {
+    File copy01 = testFolder.newFile("sample-04-01.jpg");  
+    File copy02 = testFolder.newFile("sample-04-02.jpg");      
+    FileUtils.copyFile(new File(".\\build\\resources\\test\\sample-04.jpg"), copy01);   
+    FileUtils.copyFile(new File(".\\build\\resources\\test\\sample-04.jpg"), copy02);         
+
+    SyncFile2EXIF.main(new String[] { "name", testFolder.getRoot().getAbsolutePath(), copy01.getName() });
+    SyncFile2EXIF.main(new String[] { "name", testFolder.getRoot().getAbsolutePath(), copy02.getName() });    
+
+    assertEquals(2, Arrays.stream(testFolder.getRoot().listFiles()).count());
+  }  
+
+  @Test
+  public void testMain_SetTimeNoExif() throws Exception {
+    File copy01 = testFolder.newFile("20100919-Sonia-006.jpg");     
+    FileUtils.copyFile(new File(".\\build\\resources\\test\\20100919-Sonia-006.jpg"), copy01);           
+
+    SyncFile2EXIF.main(new String[] { "setTimes", testFolder.getRoot().getAbsolutePath(), copy01.getName(), "20100919030000" });       
+
+    BasicFileAttributes attr01 = Files.readAttributes(Paths.get(copy01.getAbsolutePath()), BasicFileAttributes.class); 
+    assertEquals(Instant.parse("2010-09-19T03:00:00Z"), attr01.lastModifiedTime().toInstant());    
+    assertEquals(Instant.parse("2010-09-19T03:00:00Z"), attr01.lastAccessTime().toInstant()); 
+    assertEquals(Instant.parse("2010-09-19T03:00:00Z"), attr01.creationTime().toInstant());    
   }
 }
